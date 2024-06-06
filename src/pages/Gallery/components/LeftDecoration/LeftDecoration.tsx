dfae6edadae4394c5796588b7069d6b04093194e
Alejandro Moreno <jose.alejandro.mr@gmail.com>
2024-06-05 23:49:55 -0700

    Adding gallery page mobile version.

--- src/pages/Gallery/components/LeftDecoration/LeftDecoration.tsx
+++ src/pages/Gallery/components/LeftDecoration/LeftDecoration.tsx
@@ -3,7 +3,7 @@ import redCrossSvg from "@/assets/red-cross-left.svg";
 
 function LeftDecoration() {
   return (
-    <div className="absolute top-0 left-0 z-0 h-full w-[257px]">
+    <div className="max-lg:hidden absolute top-0 left-0 z-0 h-full w-[257px]">
       <img src={grayLineDottedSvg} className="absolute top-0 right-0" />
       <img src={redCrossSvg} className="absolute left-[88px] top-[243px]" />
     </div>
--- src/pages/Gallery/components/RightDecoration/RightDecoration.tsx
+++ src/pages/Gallery/components/RightDecoration/RightDecoration.tsx
@@ -4,7 +4,7 @@ import redLineDottedSvg from "@/assets/red-line-dotted.svg";
 
 function RightDecoration() {
   return (
-    <div className="absolute top-0 right-0 z-0 h-full w-[165px]">
+    <div className="max-lg:hidden absolute top-0 right-0 z-0 h-full w-[165px]">
       <img src={redLineSvg} className="absolute top-0 left-[29px]" />
       <img src={grayCrossSvg} className="absolute top-[125px]" />
       <img src={redLineDottedSvg} className="absolute top-[154px] right-0" />
